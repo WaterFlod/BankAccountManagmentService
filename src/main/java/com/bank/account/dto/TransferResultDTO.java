03297f727d7d3bdc8cf23f41601af50d5aa01225
Shestakov Matvey <mse25019@gmail.com>
2025-12-08 18:04:30 +0500

    fix(dto): add setter and getter TransferResultDTO.java

--- src/main/java/com/bank/account/dto/TransferResultDTO.java
+++ src/main/java/com/bank/account/dto/TransferResultDTO.java
@@ -1,7 +1,9 @@
 package com.bank.account.dto;
 
+import lombok.Data;
 import java.math.BigDecimal;
 
+@Data
 public class TransferResultDTO {
     private TransactionDTO fromTransaction;
     private TransactionDTO toTransaction;
