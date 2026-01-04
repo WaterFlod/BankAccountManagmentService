2d6044c21650ced15847c6f63b394061cc4774c4
Shestakov Matvey <mse25019@gmail.com>
2026-01-04 15:17:19 +0500

    feat(AccountServiceTest): add createAccount test
    Create test createAccountWithValidRequest_returnsValidAccountDTO().
    
    Tests the creation of an account with a valid request.

--- src/test/java/com/bank/account/unit/service/AccountServiceTest.java
+++ src/test/java/com/bank/account/unit/service/AccountServiceTest.java
@@ -1,17 +1,42 @@
 package com.bank.account.unit.service;
 
+import com.bank.account.dto.AccountDTO;
 import com.bank.account.dto.CreateAccountRequest;
 import com.bank.account.dto.TransactionRequest;
 import com.bank.account.model.Account;
+import com.bank.account.repository.AccountRepository;
+import com.bank.account.service.AccountService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
 
+    @Mock
     private Account testAccount;
+
+    @Mock
     private CreateAccountRequest createRequest;
+
+    @Mock
     private TransactionRequest transactionRequest;
+
+    @Mock
+    AccountRepository accountRepository;
+
+    @InjectMocks
+    private AccountService accountService;
 
     @BeforeEach
     void setUp() {
@@ -31,5 +56,19 @@ class AccountServiceTest {
         transactionRequest = new TransactionRequest();
         transactionRequest.setAmount(new BigDecimal("100.00"));
         transactionRequest.setDescription("Test transaction");
+    }
+
+    @DisplayName("Создание счёта с валидным запросом должно возвращать валидный AccountDTO")
+    @Test
+    void createAccountWithValidRequest_returnsValidAccountDTO() {
+        AccountDTO result = accountService.createAccount(createRequest);
+
+        assertNotNull(result);
+        assertTrue(result.getAccountNumber().startsWith("ACC"));
+        assertEquals(result.getOwnerName(), "John Doe");
+        assertEquals(result.getBalance(), new BigDecimal("500"));
+        assertEquals(result.getType(), Account.AccountType.SAVINGS);
+
+        verify(accountRepository).save(any(Account.class));
     }
 }
