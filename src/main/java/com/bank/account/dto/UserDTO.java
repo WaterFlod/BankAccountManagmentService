700fbe90e3caf9e4a950d3c384283ed1422ca0c8
Shestakov Matvey <mse25019@gmail.com>
2026-02-12 11:49:46 +0500

    feat(dto): create UserDTO

---
+++ src/main/java/com/bank/account/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.bank.account.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
