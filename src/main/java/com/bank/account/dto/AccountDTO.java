2821a3e65940bc79da678aa7e87b566b066af425
Shestakov Matvey <mse25019@gmail.com>
2026-02-12 11:45:19 +0500

    fix(dto): update type id AccountDTO

--- src/main/java/com/bank/account/dto/AccountDTO.java
+++ src/main/java/com/bank/account/dto/AccountDTO.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 @Data
 public class AccountDTO {
-    private Long id;
+    private String id;
     private String accountNumber;
     private String ownerName;
     private BigDecimal balance;
