6322fc66d0f40153cb6e432a0552978cd0c3d30f
Shestakov Matvey <mse25019@gmail.com>
2026-04-08 08:55:51 +0500

    feat: add BankAccountServiceApplication

--- src/main/java/com/bank/account/BankAccountServiceApplication.java
+++ src/main/java/com/bank/account/BankAccountServiceApplication.java
@@ -2,6 +2,9 @@ package com.bank.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @SpringBootApplication
 public class BankAccountServiceApplication {
