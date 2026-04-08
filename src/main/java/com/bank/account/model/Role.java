1e0f7bb10398edd28a8dd1b9ed6ea5093ccee601
Shestakov Matvey <mse25019@gmail.com>
2026-04-08 08:58:41 +0500

    feat(model): add Role

--- src/main/java/com/bank/account/model/Role.java
+++ src/main/java/com/bank/account/model/Role.java
@@ -6,31 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Set;
 
-@Entity
-@Table(name = "role")
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter @Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@ToString(onlyExplicitlyIncluded = true)
-public class Role  implements GrantedAuthority {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
-    @ToString.Include
-    @Setter(AccessLevel.NONE)
-    private Long id;
-
-    @Column(unique = true, nullable = false)
-    private String name;
-
-    @Transient
-    @ManyToMany(mappedBy = "roles")
-    private Set<User> users;
-
-    @Override
-    public String getAuthority() {
-        return getName();
-    }
+public enum Role {
+    USER, ADMIN
 }
