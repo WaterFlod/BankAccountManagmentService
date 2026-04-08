25db34d5f85354fceb5c2b931a590c5d5d8ceed6
Shestakov Matvey <mse25019@gmail.com>
2026-04-08 09:05:41 +0500

    feat(controller): update AuthController
    - move RestController to Controller
    - move mapping /API to /
    - add endpoint GET '/login'
    - add endpoint GET '/register'
    - add endpoint POST '/register'

--- src/main/java/com/bank/account/controller/AuthController.java
+++ src/main/java/com/bank/account/controller/AuthController.java
@@ -1,29 +1,58 @@
 package com.bank.account.controller;
 
 import com.bank.account.dto.RegistrationRequest;
-import com.bank.account.dto.UserDTO;
-import com.bank.account.service.AuthService;
+import com.bank.account.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class AuthController {
 
     @Autowired
-    private final AuthService authService;
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String loginForm(@RequestParam(value = "error", required = false) String error,
+                            Model model) {
+        if (error != null) {
+            model.addAttribute("error", "Неверное имя пользователя или пароль");
+        }
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerForm(Model model) {
+        model.addAttribute("request", new RegistrationRequest());
+        return "register";
+    }
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> register(@Validated @RequestBody RegistrationRequest request) {
-        UserDTO user = authService.createUser(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    public String registerUser(@Valid @ModelAttribute("request") RegistrationRequest request,
+                               BindingResult result,
+                               Model model) {
+        if (result.hasErrors()) {
+            return "register";
+        }
+        if (!request.getPassword().equals(request.getConfirmPassword())) {
+            model.addAttribute("confirmError", "Ваши пароли не совпадают");
+            return "register";
+        }
+        if (userService.existsUserByEmail(request.getEmail())) {
+            model.addAttribute("error", "Пользователь с таким email уже существует");
+            return "register";
+        }
+        if (userService.existsUserByPhoneNumber(request.getPhoneNumber())) {
+            model.addAttribute("error", "Пользователь с таким номером телефона уже существует");
+            return "register";
+        }
+        boolean success = userService.registerUser(request);
+        return "redirect:/login?registered";
     }
 }
